c85d8e2750d0035ca0115b51842919ec99cbe56a
Ismile Hossain <ismile52hssn@gmail.com>
2020-02-25 19:42:51 +0600

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Item from './TodoItem';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Todo list</h2>
+        <Item />
+      </div>
+    );
+  }
+}
